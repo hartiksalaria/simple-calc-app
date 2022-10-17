959828e7a58d08c49406cfe99fc3811b89f61f66
Hartik Salaria <salariahartik34@mail.com>
2022-10-17 12:03:14 +0530

    evaluation

--- calculator/static/script.js
+++ calculator/static/script.js
@@ -7,63 +7,58 @@ let b = "";
 let ope = "";
 
 const reset = () => {
-    a = "";
-    b = "";
-    ope = "";
-    res = 0;
-    result.innerHTML = res;
-}
+  a = "";
+  b = "";
+  ope = "";
+  res = 0;
+  result.innerHTML = res;
+};
 
 const number = (value) => {
-    if(ope === "")  {
-        a += value;
-        result.innerHTML = a;
-    }
-    else {
-        b += value;
-        result.innerHTML = b;
-    }
-}
+  if (ope === "") {
+    a += value;
+    result.innerHTML = a;
+  } else {
+    b += value;
+    result.innerHTML = b;
+  }
+};
 
 const compute = () => {
-    switch(ope) {
-        case '+':
-            res = parseInt(a) + parseInt(b);
-            break;
-        case '-':
-            res = parseInt(a) - parseInt(b);
-            break;
-        case '*':
-            res = parseInt(a) * parseInt(b);
-            break;
-        case '/':
-            res = parseInt(a) / parseInt(b);
-    }
-    result.innerHTML = res;
-}
+  switch (ope) {
+    case "+":
+      res = parseInt(a) + parseInt(b);
+      break;
+    case "-":
+      res = parseInt(a) - parseInt(b);
+      break;
+    case "*":
+      res = parseInt(a) * parseInt(b);
+      break;
+    case "/":
+      res = parseInt(a) / parseInt(b);
+  }
+  result.innerHTML = res;
+};
 
 const operation = (value) => {
-    ope = value;
-}
+  ope = value;
+};
 
 const decide = (value) => {
-    if(value <= "9" && value >= "0")    {
-        number(value);
-    }
-    else if(value === "C")   {
-        reset();
-    }
-
-    else if(value === "=")   {
-        compute();
-    }
-    else {
-        operation(value);
-    }
-}
+  if (value <= "9" && value >= "0") {
+    number(value);
+  } else if (value === "C") {
+    reset();
+  } else if (value === "=") {
+    compute();
+  } else {
+    operation(value);
+  }
+};
 
 buttons.forEach(function (button) {
-    button.addEventListener('click', function(){
-        decide(button.innerHTML);
-    });
+  button.addEventListener("click", function () {
+    decide(button.innerHTML);
+  });
 });
